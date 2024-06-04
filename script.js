e3925b51576084b2d8e8a3461c46dab627cde045
Hedonysos <167946166+theserendipist@users.noreply.github.com>
2024-06-04 16:39:02 +0200

    Create function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!")
+// Rock Paper Scissors Game
+
+// Function to get computer choice (either Rock, Paper, or Scissors)
+function getComputerChoice() {
+    let rndInt = Math.floor(Math.random() * 3) + 1;
+    let result;
+    if (rndInt == 1) {
+        result = "Rock";
+        return result;
+    } else if (rndInt == 2) {
+        result = "Paper";
+        return result;
+    } else if (rndInt == 3) {
+        result = "Scissors";
+        return result;
+    }
+}
+
