a206f677ed974b9e19e1dab4a6f191640714c07d
Hedonysos <167946166+theserendipist@users.noreply.github.com>
2024-07-30 23:01:57 +0200

    Add buttons to record human choice

--- script.js
+++ script.js
@@ -16,41 +16,41 @@ function getComputerChoice() {
     }
 }
 
-// Function to get human choice
-function getHumanChoice() {
-    let choice = prompt("What's your pick? Rock, Paper or Scissors?").toLowerCase();
-    if (choice == "rock" || choice == "paper" || choice == "scissors") {
-        return choice;
-    } else {
-        alert("This is not a valid pick! Please type eiher Rock, Paper, or Scissors!");
-        getHumanChoice();
-    }
-}
-
 // Function to play one single round
+let humanChoice;
+
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
 
     if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You win! Rock beats Scissors.");
-        return humanScore++;
     } else if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose! Paper beats Rock.");
-        return computerScore++;
     } else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You win! Paper beats Rock.");
-        return humanScore++;
     } else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lose! Scissors beats Paper.");
-        return computerScore++;
     } else if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You win! Scissors beats Paper.");
-        return humanScore++;
     } else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lose! Rock beats Scissors.");
-        return computerScore++;
     } else {
         console.log("It's a draw!");
     }
 }
+
+//Event listeners to record human choice and play one round
+const rckBtn = document.querySelector("#rck-btn");
+rckBtn.addEventListener("click", () => {
+    playRound(humanChoice = "rock");
+});
+
+const pprBtn = document.querySelector("#ppr-btn");
+pprBtn.addEventListener("click", () => {
+    playRound(humanChoice = "paper");
+});
+
+const scissBtn = document.querySelector("#sciss-btn");
+scissBtn.addEventListener("click", () => {
+    playRound(humanChoice = "scissors");
+});
